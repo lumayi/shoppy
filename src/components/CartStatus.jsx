e2c24e32420f95f7f04304c64b842804efe26307
greeneeya <suzythetravelerr@gmail.com>
2023-08-09 21:52:38 +0900

    cartStatus query 적용

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -1,11 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { getCartProducts } from '../api/product/products';
 
 export default function CartStatus({ cartStatus, onClick }) {
+  const { data } = useQuery(['cart'], () => getCartProducts());
   return (
     <button type="button" className="relative" onClick={onClick}>
       <img src="image/cartImage.svg" alt="cart" className="w-6" />
       <div className="bg-red-700 absolute top-[-5px] right-[-5px] text-white rounded-full text-xs w-4 h-4">
-        {cartStatus}
+        {data && Object.keys(data).length}
       </div>
     </button>
   );
